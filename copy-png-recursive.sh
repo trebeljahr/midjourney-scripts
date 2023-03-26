6291ed20aa9a03a69ee84a34a30e7a03054f959e
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-26 21:33:47 +0200

    Add next draft for copying script

--- copy-png-recursive.sh
+++ copy-png-recursive.sh
@@ -13,15 +13,27 @@ dest_dir="$2"
 # Create the destination directory if it doesn't exist
 mkdir -p "$dest_dir"
 
-# Initialize file counter
-counter=1
-
-# Find all PNG files recursively and copy them to the destination directory with new names
-find "$src_dir" -iname '*.png' -type f | while read -r file; do
-  new_filename=$(printf "%04d.png" $counter)
-  cp "$file" "$dest_dir/$new_filename"
-  echo "Copied $file to $dest_dir/$new_filename"
-  ((counter++))
+# Find all JSON files recursively and process them
+find "$src_dir" -iname '*.json' -type f | while read -r json_file; do
+  # Get the corresponding PNG file path
+  png_file="${json_file%.*}.png"
+  
+  # Check if the PNG file exists
+  if [ -f "$png_file" ]; then
+    # Extract the prompt from the JSON file
+    prompt=$(jq -r '.prompt' "$json_file")
+    number=$(jq -r '.id' "$json_file")
+    
+    # Replace spaces with underscores and remove special characters
+    new_filename=$(echo "$prompt" | tr ' ' '_' | tr -cd '[:alnum:]_')
+    new_filename="${new_filename}---${id}.png"
+    
+    # Copy the PNG file to the destination directory with the new name
+    cp "$png_file" "$dest_dir/$new_filename"
+    echo "Copied $png_file to $dest_dir/$new_filename"
+  else
+    echo "Warning: PNG file not found for $json_file"
+  fi
 done
 
 echo "PNG files copied from $src_dir to $dest_dir"
